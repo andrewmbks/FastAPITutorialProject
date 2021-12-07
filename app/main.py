183e7978babbed148463ee869d7056f7b7580fa0
Andrew Miller <andrewm@britannica-ks.com>
2021-12-07 13:08:00 -0600

    committing changes

--- app/main.py
+++ app/main.py
@@ -18,33 +18,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Database Connection
-# while True:
-#     try:
-#         connection = psycopg2.connect(host='localhost', database='FastAPI', user='postgres', password='password', cursor_factory=RealDictCursor)
-#         cursor = connection.cursor()
-#         print("Database connection was successful!")
-#         break
-#     except Exception as error:
-#         print("Connecting to database failed")
-#         print("Error:", error)
-#         time.sleep(2)
-
-
-# my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "me likey pizza", "id": 2}]
-
-
-# def find_post(id):
-#     for p in my_posts:
-#         if p['id'] == id:
-#             return p
-
-# def find_index_post(id):
-#     for i, p in enumerate(my_posts):
-#         if p['id'] == id:
-#             return i
-
-
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
