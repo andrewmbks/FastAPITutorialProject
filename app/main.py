668b31dced795aea71dc8efb07bfe2c6a9dfa5dc
Andrew Miller <andrewm@britannica-ks.com>
2021-12-07 13:04:54 -0600

    committing changes vid time - 7:41:20

--- app/main.py
+++ app/main.py
@@ -1,80 +1,54 @@
 #vid URL: https://www.youtube.com/watch?v=0sOvCWFmrtA
 
-from typing import Optional
-from fastapi import FastAPI, Response, status, HTTPException
+from typing import Optional, List
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.param_functions import Body
-from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from sqlalchemy.orm import Session
+from . import models, schemas, utils
+from .database import engine, get_db
+from .routers import post, user, auth
 
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+# Database Connection
+# while True:
+#     try:
+#         connection = psycopg2.connect(host='localhost', database='FastAPI', user='postgres', password='password', cursor_factory=RealDictCursor)
+#         cursor = connection.cursor()
+#         print("Database connection was successful!")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error:", error)
+#         time.sleep(2)
 
-class Post(BaseModel):
-    title: str 
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
+
+# my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "me likey pizza", "id": 2}]
 
 
-my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {"title": "favorite foods", "content": "me likey pizza", "id": 2}]
+# def find_post(id):
+#     for p in my_posts:
+#         if p['id'] == id:
+#             return p
+
+# def find_index_post(id):
+#     for i, p in enumerate(my_posts):
+#         if p['id'] == id:
+#             return i
 
 
-def find_post(id):
-    for p in my_posts:
-        if p['id'] == id:
-            return p
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
-
+app.include_router(post.router)
+app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to my API"}
-
-
-@app.get("/posts")
-def get_posts():
-    return {"data": my_posts}
-
-
-@app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 10000000)
-    my_posts.append(post_dict)
-    return{"data": post_dict}
-
-
-@app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} was not found.")
-    return{"post_detail": post}
-
-@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
-def delete_post(id: int):
-    # deleteing post
-    index = find_index_post(id)
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist.")
-    my_posts.pop(index)
-    return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-@app.put("/posts/{id}")
-def update_post(id: int, post: Post):
-    index = find_index_post(id)
-
-    if index == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist.")
-        
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    print(post)
-    return{"data": post_dict}
