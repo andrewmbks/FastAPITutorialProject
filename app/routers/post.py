0715b2abc7424d09264b79b77614c12316f5bc7f
Andrew Miller <andrewm@britannica-ks.com>
2021-12-08 17:19:04 -0600

    commiting changes for the day vid time: 9:52:51

--- app/routers/post.py
+++ app/routers/post.py
@@ -15,8 +15,7 @@ router = APIRouter(
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
-    print("limit:", limit)
-    print("skip:", skip)
+
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
